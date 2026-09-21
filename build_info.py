ca9581ef028c8e663acd73bec15179041a22d0ec
Xiu Jin <coderxiu@qq.com>
2026-09-21 19:44:52 +0800

    chore: stamp v3.6 0.11.32 build

--- build_info.py
+++ build_info.py
@@ -1,5 +1,5 @@
 """Release identity shared by the V3.6 Python backend and package checks."""
 
 APP_EDITION = "V3.6"
-APP_VERSION = "0.11.31"
-BUILD_COMMIT = "3d759e3"
+APP_VERSION = "0.11.32"
+BUILD_COMMIT = "b71a24d"
